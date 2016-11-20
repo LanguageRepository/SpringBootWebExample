3c2efe20a419b4d8c9436b8c70b0cdf46a92f0f8
LanguageRepository <vladyslav.bublyk@nure.ua>
2016-11-20 12:53:56 +0200

    EmployeeService added.

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,37 @@
+package com.example.service;
+
+import com.example.dao.EmployeeDao;
+import com.example.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    public EmployeeService() { }
+
+    @Transactional
+    public Employee createEmployee(Employee employee) {
+        return employeeDao.create(employee);
+    }
+
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDao.read(id);
+    }
+
+    @Transactional
+    public void updateEmployee(Employee employee) {
+        employeeDao.update(employee);
+    }
+
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDao.delete(id);
+    }
+
+}
